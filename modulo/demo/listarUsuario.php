a3bfd5ed0d37e4d6150807e7a222be360b208096
Marcio Ramos <marciomrs4@hotmail.com>
2015-06-24 22:06:59 -0300

    Criado a class SelectOption

--- modulo/demo/listarUsuario.php
+++ modulo/demo/listarUsuario.php
@@ -11,13 +11,20 @@ $lista = new demo\repository\UsuarioRepository;
 $grid = new \Mrs\core\Grid();
 
 $grid->setDados($lista->listarUsuarios())
-     ->setCabecalho(['Nome','Email','Ramal','Login'])
-     ->show();
+     ->setCabecalho(['','Nome','Email','Ramal','Login']);
 
 
 
+$ButtonOption = new \Mrs\core\GridOption();
+
+$ButtonOption->setIco('search')->setName('Procurar');
+
+$grid->addOption($ButtonOption);
 
 
+$Painel = new \Mrs\core\Painel();
+$Painel->addGrid($grid)->setPainelTitle('Lista de Usuario')
+       ->setPainelColor('default')->show(!isset($_SESSION['action']));
 
 
 
